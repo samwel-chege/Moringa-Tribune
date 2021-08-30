5648183db5e350e0af9a924f6963230f9528f469
samwel-chege <samwelchegeh09@gmail.com>
2021-08-30 11:48:56 +0300

    create a convert_dates function that takes a date as an object

--- news/views.py
+++ news/views.py
@@ -16,4 +16,15 @@ def news_of_day(request):
             </body>
         </html>        
             '''   
-    return HttpResponse(html)         
+    return HttpResponse(html) 
+
+def convert_dates(dates):
+
+    #Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    #returning the actual day of the week
+    day = days[day_number]   
+    return day         
