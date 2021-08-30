a187372a82e687e031ae9077c4402421bcb6a703
samwel-chege <samwelchegeh09@gmail.com>
2021-08-30 12:59:24 +0300

    call the render function and pass in the request and the template file

--- news/views.py
+++ news/views.py
@@ -6,7 +6,7 @@ import datetime as dt
 # Create your views here.
 
 def welcome(request):
-    return  HttpResponse('Welcome to the Moringa Tribune')
+    return  render(request,'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
