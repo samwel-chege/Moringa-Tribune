7e35c9ab6733eecd294fe5ff20010ec53a993fdb
samwel-chege <samwelchegeh09@gmail.com>
2021-08-31 17:13:23 +0300

    write test files

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Editor,tags,Article
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.sammie= Editor(first_name = 'Sammie',last_name= 'Sam',email = 'sammiesam@gmail.com')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sammie,Editor))   
+
+    #testing save method
+    def test_save_method(self):
+        self.sammie.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)> 0)    
