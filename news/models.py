8558a493e8da89893803a753b6d738f7a53c6fd0
samwel-chege <samwelchegeh09@gmail.com>
2021-08-31 17:55:19 +0300

    add a new editor field

--- news/models.py
+++ news/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
 class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=10,blank=True)
 
     def __str__(self):
         return self.first_name
